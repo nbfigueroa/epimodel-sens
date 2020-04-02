034e96477c9175b683ef568a7651a91a485980c9
nbfigueroa <nbfigueroa@gmail.com>
2020-04-02 05:43:02 -0400

    Playing around with sensitivity analysis of vanilla SIR

--- sim_vanillaSIR_model.py
+++ sim_vanillaSIR_model.py
@@ -1,9 +1,10 @@
 import numpy as np
-from scipy.integrate import odeint
-from scipy.optimize import fsolve
-
+from   scipy.integrate import odeint
+from   scipy.optimize import fsolve
+from   scipy import stats
 import matplotlib.pyplot as plt
 from matplotlib import rc
+
 rc('font',**{'family':'serif','serif':['Times']})
 rc('text', usetex=True)
 
@@ -17,7 +18,7 @@ days = 90
 # Contact rate (beta), and mean recovery rate (gamma) (in 1/days).
 # Estimtated values
 gamma_inv  = 4.5  # SEIRS dudes: 6.8, Luis B.: 4.5, 1/0.3253912
-r0         = 2.25 # Basic Reproductive Rate 2.65, estimates between 2.2-6.5
+r0         = 4 # Basic Reproductive Rate 2.65, estimates between 2.2-6.5
 beta       = r0 / gamma_inv
 gamma      = 1.0 / gamma_inv
 
@@ -42,6 +43,86 @@ def deriv(y, t, N, beta, gamma):
     dRdt = gamma * I
     return dSdt, dIdt, dRdt
 
+#############################################################
+######## Dependence of R0 on Final Epidemic Behavior ########
+#############################################################
+
+# Final epidemic size (analytic)
+r0_vals     = np.linspace(1,5,100) 
+init_guess  = 0.0001
+Sinf_N      =   []
+Sinf_S0     =   []
+for ii in range(len(r0_vals)):
+    r0_test = r0_vals[ii]
+    Sinf_N.append(fsolve(epi_size, init_guess))     
+    Sinf_S0.append(1 - Sinf_N[ii])
+
+r0_test      = r0
+covid_SinfN  = fsolve(epi_size, init_guess)
+covid_SinfS0 = 1 - covid_SinfN
+print('Covid r0 = ', r0_test, 'Covid Sinf/S0 = ', covid_SinfN[0], 'Covid Sinf/S0 = ', covid_SinfS0[0]) 
+
+# Plots
+fig0, ax0 = plt.subplots()
+ax0.plot(r0_vals, Sinf_S0, 'r', lw=2, label='Susceptible')
+ax0.set_ylabel('Sinf/S0 (percentage of population infected)')
+ax0.set_xlabel('R0')
+
+# Current estimate of Covid R0
+plt.title('Final Size of Epidemic Dependence on R0 estimate',fontsize=15)
+ax0.plot(r0_test, covid_SinfS0, 'ko', markersize=5, lw=2)
+
+# Plot mean
+txt = 'Covid R0({r0:3.3f})'
+ax0.text(r0_test - 0.45, covid_SinfS0 + 0.05,txt.format(r0=r0_test), fontsize=10)
+plt.plot([r0]*10,np.linspace(0,covid_SinfS0,10), color='black')
+txt = "{Sinf:3.3f} Infected"
+ax0.text(1.1, covid_SinfS0 - 0.025,txt.format(Sinf=covid_SinfS0[0]), fontsize=8)
+plt.plot(np.linspace(1,[r0],10), [covid_SinfS0]*10, color='black')
+
+# Sample R0 values from Gaussian distribution for Covid estimate
+mu_r0, sigma_r0 = r0, 0.15  # mean and standard deviation
+r0_gauss   = stats.norm.pdf(r0_vals,mu_r0,sigma_r0)
+r0_samples = np.random.normal(mu_r0, sigma_r0, 25)
+covid_SinfS0_samples  =   []
+for ii in range(len(r0_samples)):
+    r0_test = r0_samples[ii]
+    covid_SinfS0_samples.append(1 - fsolve(epi_size, init_guess))
+
+plt.plot(r0_samples, covid_SinfS0_samples, 'ko', markersize=5, lw=2, alpha=0.5)
+
+# Plot +std
+r0_test          = r0+2*sigma_r0
+covid_SinfS0_plus = 1 - fsolve(epi_size, init_guess)
+ax0.text(r0_test + 0.02, covid_SinfS0_plus - 0.15,r'$R_0+2\sigma$', fontsize=10)
+txt = '$\sigma$=' + str(sigma_r0)
+ax0.text(r0_test + 0.02, covid_SinfS0_plus - 0.5,txt, fontsize=10)
+plt.plot([r0_test]*10,np.linspace(0,covid_SinfS0_plus,10), color='black', alpha=0.5)
+txt = "{Sinf:3.3f} Infected"
+ax0.text(1.1, covid_SinfS0_plus + 0.01,txt.format(Sinf=covid_SinfS0_plus[0]), fontsize=8)
+plt.plot(np.linspace(1,[r0_test],10), [covid_SinfS0_plus]*10, color='black', alpha=0.5)
+
+
+# Plot -std
+r0_test          = r0-2*sigma_r0
+covid_SinfS0_plus = 1 - fsolve(epi_size, init_guess)
+ax0.text(r0_test - 0.25, covid_SinfS0_plus - 0.15,r'$R_0-2\sigma$', fontsize=10)
+plt.plot([r0_test]*10,np.linspace(0,covid_SinfS0_plus,10), color='black', alpha=0.5)
+txt = "{Sinf:3.3f} Infected"
+ax0.text(1.1, covid_SinfS0_plus - 0.05,txt.format(Sinf=covid_SinfS0_plus[0]), fontsize=8)
+plt.plot(np.linspace(1,[r0_test],10), [covid_SinfS0_plus]*10, color='black', alpha=0.5)
+
+ax0.xaxis.set_tick_params(length=0)
+ax0.yaxis.set_tick_params(length=0)
+
+# Plot Gaussian and sampled from R0
+ax01       = ax0.twinx()   # instantiate a second axes that shares the same x-axis
+plt.plot(r0_vals,r0_gauss/mu_r0, color='black', alpha=0.5)
+plt.plot(r0_samples, [0.15]*len(r0_samples), 'ko', markersize=5, lw=2, alpha=0.5)
+plt.ylim(0.1,5)
+ax01.xaxis.set_tick_params(length=0)
+ax01.yaxis.set_tick_params(length=0)
+
 
 ###################################################
 ######## Initial Parameters for Simulation ########
@@ -63,39 +144,14 @@ y0 = S0, I0, R0
 ode_sol = odeint(deriv, y0, t, args=(N, beta, gamma))
 S, I, R = ode_sol.T
 
-max_inf_idx = np.argmax(I)
-max_inf     = I[max_inf_idx]
-
-# Final epidemic size (analytic)
-r0_vals     = np.linspace(1,5,25) 
-init_guess  = 0.0001
-Sinf_N      =   []
-Sinf_S0     =   []
-for ii in range(len(r0_vals)):
-	r0_test = r0_vals[ii]
-	Sinf_N.append(fsolve(epi_size, init_guess))		
-	Sinf_S0.append(1 - Sinf_N[ii])
-
-
-
-fig0, ax0 = plt.subplots()
-ax0.plot(r0_vals, Sinf_S0, 'r', lw=2, label='Sinf/S0')
-ax0.set_ylabel('Sinf/S0 (percentage of population infected)')
-ax0.set_xlabel('R0')
-plt.title('Final Size of Epidemic Dependence on R0 estimate',fontsize=15)
-
-r0_test      = 2.65
-covid_SinfN  = fsolve(epi_size, init_guess)
-covid_SinfS0 = 1 - covid_SinfN
-print('Covid r0 = ', r0_test, 'Covid Sinf/S0 = ', covid_SinfN, 'Covid Sinf/S0 = ', covid_SinfS0) 
-
 # Analytic growth rate
 effective_Rt = r0 * (S/N)
 growth_rates = gamma * (effective_Rt - 1)
 
 print('*****   Results    *****')
-print('Maximum infected = ', max_inf,'by day=',max_inf_idx)
-
+max_inf_idx = np.argmax(I)
+max_inf     = I[max_inf_idx]
+print('Turning point Infected = ', max_inf,'by day=',max_inf_idx)
 
 ##################################
 ######## Plots Generation ########
@@ -103,7 +159,7 @@ print('Maximum infected = ', max_inf,'by day=',max_inf_idx)
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig, (ax1, ax2) = plt.subplots(1,2)
 
-txt_title = "COVID-19 SIR Model Dynamics (N={N:2.0f},R0={R0:1.3f},1/gamma={gamma_inv:1.3f}, beta={beta:1.3f})"
+txt_title = "COVID-19 Vanilla SIR Model Dynamics (N={N:2.0f},R0={R0:1.3f},1/gamma={gamma_inv:1.3f}, beta={beta:1.3f})"
 fig.suptitle(txt_title.format(N=N, R0=r0, gamma_inv = gamma_inv, beta= beta),fontsize=15)
 
 # Variable evolution
@@ -111,18 +167,17 @@ ax1.plot(t, S/N, 'k', lw=2, label='Susceptible')
 ax1.plot(t, I/N, 'r', lw=2, label='Infected')
 ax1.plot(t, R/N, 'g', lw=2, label='Recovered')
 
-# Max infected
+# Inflection point
 ax1.plot(max_inf_idx, max_inf/N, 'md', markersize=12, lw=2)
-txt = "Max infected ({max:5.0f}) @ {day:3.0f} days "
-ax1.text(max_inf_idx + 5, max_inf/N - 0.05,txt.format(max=max_inf, day=max_inf_idx), fontsize=12)
+txt = "Tipping point @ {day:3.0f} days "
+ax1.text(max_inf_idx + 5, max_inf/N - 0.05,txt.format(day=max_inf_idx), fontsize=12)
+ax1.plot(t, covid_SinfS0*np.ones(len(t)), 'm--')
+txt1 = "{per:2.2f} population infected"
+ax1.text(t[0], covid_SinfS0 - 0.05, txt1.format(per=covid_SinfS0[0]), fontsize=12, color='m')
+
 
 ax1.set_xlabel('Time /days')
 ax1.set_ylabel('Percentage of Population')
-percs = max_inf/N*np.ones(len(t))
-ax1.plot(t, percs, 'k--')
-txt1 = "{per:2.2f}"
-ax1.text(t[0], max_inf/N - 0.05,txt1.format(per=max_inf/N), fontsize=12)
-
 ax1.yaxis.set_tick_params(length=0)
 ax1.xaxis.set_tick_params(length=0)
 ax1.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -147,6 +202,11 @@ legend.get_frame().set_alpha(0.5)
 for spine in ('top', 'right', 'bottom', 'left'):
     ax2.spines[spine].set_visible(True)
 
+
+plt.show()
+
+
+
 # Plot Grow Rate
 # fig, (ax1, ax2) = plt.subplots(1,2)
 # txt_title = "COVID-19 SIR Model Dynamics (N={N:2.0f},R0={R0:1.3f},1/gamma={gamma_inv:1.3f}, beta={beta:1.3f})"
@@ -158,12 +218,6 @@ for spine in ('top', 'right', 'bottom', 'left'):
 # ax2.plot(t, effective_Rt, 'k', lw=2, label='Rt (Effective Reproductive Rate)')
 # ax2.set_ylabel('Rt (Effective Reproductive Rate)')
 # ax2.set_xlabel('t')
-
-
-
-plt.show()
-
-
 
 # fig, ax = plt.subplots()
 # ax.plot(S, I, 'k', lw=2, label='Susceptible')
